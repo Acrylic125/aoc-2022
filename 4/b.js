95e3fcd6ebfb3d949b8acb6fe00eba45eff8af61
Acrylic125 <acrylic125email@gmail.com>
2022-12-04 13:09:47 +0800

    copilot is op

---
+++ 4/b.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+function parseRange(str) {
+  return str.split("-").map((v) => parseInt(v));
+}
+
+(async () => {
+  const data = await fs.promises.readFile("data.txt", "utf8");
+  const lines = data.split("\n");
+  let count = 0;
+
+  lines.forEach((line) => {
+    if (line === "") return;
+    const [p1, p2] = line.split(",");
+
+    const [min1, max1] = parseRange(p1);
+    const [min2, max2] = parseRange(p2);
+
+    // Check if line 1 overlaps with line 2.
+    if (min1 <= max2 && max1 >= min2) {
+      count++;
+      return;
+    }
+    // Check if line 2 overlaps with line 1.
+    if (min2 <= max1 && max2 >= min1) {
+      count++;
+      return;
+    }
+  });
+  console.log(count);
+})();
