c89e6b46dec112fdea3392dc4a9713a83c38914e
Acrylic125 <acrylic125email@gmail.com>
2022-12-06 13:38:37 +0800

    this sucks I misunderstood the qns again

---
+++ 6/b.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+(async () => {
+  const data = await fs.promises.readFile("data.txt", "utf8");
+  const lines = data.split("\n");
+
+  const dataStream = lines[0];
+
+  // const uniqueChars = new Set();
+  // const markers = new Set();
+  const streamChars = dataStream.split("");
+
+  console.log(dataStream);
+  var s = 0;
+  // Find the first unique character after the first 4 characters.
+  for (let i = 14; i < streamChars.length; i++) {
+    // const char = streamChars[i];
+    const prevChars = streamChars.slice(i - 14, i);
+    const uniqueChars = new Set(prevChars);
+    if (uniqueChars.size === 14) {
+      console.log(i);
+      return;
+    }
+
+    // markers.add(char);
+    // uniqueChars.add(char);
+
+    // console.log(`${s} ${markers.size}: ${char}}`);
+    // if (markers.size > 4) {
+    //   console.log(s - 1);
+    //   return;
+    // }
+  }
+  // for (const char of streamChars) {
+  //   s++;
+  //   if (uniqueChars.size > 4) {
+  //     console.log(s);
+  //     return;
+  //   }
+  //   uniqueChars.add(char);
+  //   console.log(uniqueChars);
+  // }
+})();
